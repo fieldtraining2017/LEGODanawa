8ea9a537ec7c426d9b81b26bf1e305ac054fa661
Cheechyo <kwj9211@gmail.com>
2017-02-08 15:23:12 +0900

    brickdata

---
+++ data/brlicklist_test.js
@@ -0,0 +1,7 @@
+var bricklist = [
+	{id:"10039c01", ldd:"10039", name:"Pullback Motor 8 x 4 x 2/3"},
+	{id:"10049pb01", ldd:"10049p01", name:"Minifig, Shield Broad with Spiked Bottom and Cutout Corner with Handprint Pattern"},
+	{id:"10051pb01", ldd:"10051p01", name:"Minifig, Headgear Helmet Castle with Lateral Comb and Handprint Pattern"},
+	{id:"10054pb01", ldd:"10054p01", name:"Minifig, Headgear Helmet Castle with Cheek Protection and Comb with Gold Trim Pattern (Eomer)"},
+	{id:"10054pb02", ldd:"10054p02", name:"Minifig, Headgear Helmet Castle with Cheek Protection and Comb with Gold Icons Pattern (King Theoden)"}
+];
